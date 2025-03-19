7c4bfffe21df0fc7957d4728b358be286a17e96d
Jhonn Alvarez <jsalvarezmolina@student.fullsail.edu>
2025-03-18 19:23:32 -0700

    Added error messages module

---
+++ app/messages/messages.js
@@ -0,0 +1,11 @@
+// messages.js
+module.exports = {
+    CAR_NOT_FOUND: "Car not found.",
+    MANUFACTURER_NOT_FOUND: "Manufacturer not found.",
+    NO_MANUFACTURERS: "No manufacturers saved.",
+    MANUFACTURER_CREATED: "Manufacturer created and linked to car.",
+    MANUFACTURER_UPDATED: "Manufacturer updated successfully.",
+    MANUFACTURER_DELETED: "Manufacturer deleted successfully.",
+    ERROR: "An error occurred.",
+  };
+  
