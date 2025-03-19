ad659f545143214452e5204f70f6712c0ca32522
Jhonn Alvarez <jsalvarezmolina@student.fullsail.edu>
2025-03-18 19:24:00 -0700

    Update manufacturer.js
    No changes made

--- app/models/manufacturer.js
+++ app/models/manufacturer.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
 
-const manufacturerSchema = new mongoose.Schema({ 
-    name: {
-        type: String,
-        required: [true, 'Please provide a name'],
-        unique: [true, 'Name already exists'],
-        trim: true
-    },
-    location: {
-        type: String,
-        required: [true, 'Please provide a location'],
-        unique: [true, 'Location already exists']
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
+const manufacturerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    unique: [true, 'Name already exists'],
+    trim: true
+  },
+  location: {
+    type: String,
+    required: [true, 'Please provide a location'],
+    unique: [true, 'Location already exists']
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  cars: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Car' 
+  }]
 });
 
-module.exports = mongoose.model('manufacturer', manufacturerSchema);
+module.exports = mongoose.model('Manufacturer', manufacturerSchema);
