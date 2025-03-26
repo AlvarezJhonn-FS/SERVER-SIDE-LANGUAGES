1218aa06a790764c83b17bcf89a26f6ab50d3a8e
Jhonn Alvarez <jsalvarezmolina@student.fullsail.edu>
2025-03-25 17:49:17 -0700

    Updated model

--- app/models/Cars.js
+++ app/models/Cars.js
@@ -1,43 +1,40 @@
 const mongoose = require('mongoose');
 
-
 const carSchema = new mongoose.Schema({
-    make: {
-        type: String,
-        required: [true, 'Please provide a make'],
-        trim: true
-    },
-    model: {
-        type: String,
-        required: [true, 'Please provide a model'],
-        unique: [true, 'Model already exists']
-    },
-    year: {
-        type: Number,
-        required: [true, 'Please provide a year'],
-       
-    },
-    seller: {
-        type: String,
-        required: [true, 'Please provide a seller']
-    },
-    price: {
-        type: Number,
-        required: [true, 'Please provide a price']
-    },
-    color: {
-        type: String,
-        required: [true, 'Please provide a color'],
-        unique: [true, 'Color already exists']
-    },
-    manufacturer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Manufacturer',
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
+  make: {
+    type: String,
+    required: [true, 'Please provide a make'],
+    trim: true
+  },
+  model: {
+    type: String,
+    required: [true, 'Please provide a model'],
+
+  },
+  year: {
+    type: Number,
+    required: [true, 'Please provide a year'],
+  },
+  seller: {
+    type: String,
+    required: [true, 'Please provide a seller']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please provide a price']
+  },
+  color: {
+    type: String,
+    required: [true, 'Please provide a color'],
+  },
+  manufacturer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Manufacturer',
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Car', carSchema);
