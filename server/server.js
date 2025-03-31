174420608fc9bf106d5a1c6301d1d7c0834fe2fd
Jhonn Alvarez <jsalvarezmolina@student.fullsail.edu>
2025-03-30 19:25:48 -0700

    Configure server to connect to database and set up CORS
    - Loaded environment variables using dotenv for configuration.
    - Set up CORS to allow requests from `http://localhost:3000` with specific headers and methods.
    - Established a database connection using `connectDB` and handled connection errors.
    - Configured server to listen on the specified port and log successful startup.
    - Used a fallback port (3000) if the environment variable `PORT` is not defined.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const dotenv = require("dotenv").config();
 const connectDB = require("./app/config/db");
 const app = require("./app");
+const cors = require("cors");
 
 
-connectDB();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  })
+);
 
-const PORT = process.env.PORT || 3000;
-console.log("process.env.PORT", process.env.PORT);
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    console.log("process.env.PORT", process.env.PORT);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1); 
+  });
